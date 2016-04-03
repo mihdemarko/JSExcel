996baa8714c0e7513922537c50e92f2366375940
mihdemarko <mihdemarko@gmail.com>
2016-04-03 21:16:14 +0300

    Completely rewritred main.js. Created tableUI, column names function, infinite table, and other stuff

--- js/main.js
+++ js/main.js
@@ -1,195 +1,260 @@
-function Element(name, tag, inner, className, parentElement) {
-  this.name = name;
-  this.node=document.createElement(tag);
-  if (name === 'input') {this.node.value = inner;}
-  if (inner) {this.node.innerHTML = inner;}
-  if (className) {this.node.className = className;}
-  if (parentElement) {
-    if (typeof parentElement === 'string'){
-      document.getElementById(parentElement).appendChild(this.node);
-    } else if (parentElement.hasOwnProperty('node')){
-      parentElement.node.appendChild(this.node);
-    } else {
-      parentElement.appendChild(this.node);
-    }
-  }
-}
+"use strict"
 
-Element.prototype.addEvent = function(event,func){
-  this.node.addEventListener(event, func);
+var tableUI = {
+  alphabet: {
+    alphabet:'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split(''),
+    makeName:function  (num, str) {
+      if (isNaN(num)) {return null;}
+      if (!str) {str = '';}
+      if (num < 26) {
+        str = this.alphabet[num] + str;
+        return str;
+      } else {
+        str = this.alphabet[num%26] + str;
+        return this.makeName (Math.floor(num/26)-1, str);
+      }
+    },
+  },
+
+  table: {
+    tag: 'table',
+    addEvent: function (eventName, func){
+      if (this.node) {
+        this.node.addEventListener(eventName, func());
+      }
+    }
+  },
+  row: {
+    tag: 'tr',
+    addEvent: function (eventName, func){
+      if (this.node) {
+        this.node.addEventListener(eventName, func());
+      }
+    }
+  },
+  cell: {
+    tag: 'td',
+    addEvent: function (eventName, func){
+      if (this.node) {
+        this.node.addEventListener(eventName, func);
+      }
+    }
+  },
+  col: {},
+  input:{
+    tag: 'input'
+  },
+
+  mouseEvents: {
+    cellClick: function (event){
+      return function (event) {
+        if (this.getAttribute("active") !== 'true'){
+          tableUI.mouseEvents.disableActive ();
+          this.setAttribute('active', 'true');
+          tableUI.cell.lastNode = this;
+          if (this.childNodes.length === 0){
+            tableUI.create('input', this);
+            tableUI.input.node.focus();
+          } else {
+            if (this.childNodes[0].data){
+              var text = this.childNodes[0].data;
+              this.removeChild(this.childNodes[0]);
+              tableUI.create('input', this);
+              tableUI.input.node.value = text;
+              tableUI.input.node.focus();
+              }
+            }
+          }
+        };
+    },
+
+    rowClick:function (event){
+      return function (event) {
+        if (this.parentNode.getAttribute("active") !== 'true'){
+          tableUI.mouseEvents.disableActive ();
+          this.parentNode.setAttribute('active', 'true');
+          tableUI.row.lastNode = this.parentNode;
+        }
+      };
+    },
+
+    colClick: function (event){
+      return function (event) {
+        var col = document.getElementsByClassName(this.className);
+        if (col[1].getAttribute("active-col") !== 'true'){
+          tableUI.mouseEvents.disableActive ();
+          for (var i = 1; i < col.length; i += 1){
+            col[i].setAttribute('active-col', 'true');
+          }
+        tableUI.col.lastCol = col;
+        }
+      };
+    },
+
+    disableActive: function () {
+      if (tableUI.row.lastNode) {
+        tableUI.row.lastNode.setAttribute('active', 'false');
+      }
+      if (tableUI.cell.lastNode) {
+        tableUI.cell.lastNode.setAttribute('active', 'false');
+        if (tableUI.cell.lastNode.childNodes[0]){
+          var text = tableUI.cell.lastNode.childNodes[0].value || tableUI.cell.lastNode.childNodes[0].data;
+          tableUI.cell.lastNode.removeChild(tableUI.cell.lastNode.childNodes[0]);
+          if (text) {
+            tableUI.cell.lastNode.appendChild(document.createTextNode(text));
+          }
+        }
+      }
+      if (tableUI.col.lastCol) {
+        for (var i = 1; i < tableUI.col.lastCol.length; i += 1){
+          tableUI.col.lastCol[i].setAttribute('active-col', 'false');
+        }
+      }
+    }
+  },
+
+  keyCodes:{
+    ENTER: 13,
+    RIGHT_ARROW: 39,
+    LEFT_ARROW: 37,
+    UP_ARROW: 38,
+    DOWN_ARROW: 40
+  },
+
+  keyboardEvents:{
+    onCells:function (event){
+      return function (event) {
+        if (event.keyCode === tableUI.keyCodes.ENTER) {
+          tableUI.mouseEvents.disableActive(this);
+        } else if (event.keyCode === tableUI.keyCodes.DOWN_ARROW) {
+          tableUI.mouseEvents.disableActive(this);
+          tableUI.keyboardEvents.rowNum = parseInt(this.parentElement.className) + 1;
+          tableUI.keyboardEvents.column = document.getElementsByClassName(this.className);
+          tableUI.mouseEvents.cellClick ().bind(tableUI.keyboardEvents.column[tableUI.keyboardEvents.rowNum])();
+        } else if (event.keyCode === tableUI.keyCodes.UP_ARROW) {
+          tableUI.mouseEvents.disableActive(this);
+          tableUI.keyboardEvents.rowNum = parseInt(this.parentElement.className);
+          if (tableUI.keyboardEvents.rowNum > 1) {
+            tableUI.keyboardEvents.rowNum -= 1;
+            tableUI.keyboardEvents.column = document.getElementsByClassName(this.className);
+            tableUI.mouseEvents.cellClick ().bind(tableUI.keyboardEvents.column[tableUI.keyboardEvents.rowNum])();
+          }
+        } else if (event.keyCode === tableUI.keyCodes.LEFT_ARROW) {
+          tableUI.mouseEvents.disableActive(this);
+          tableUI.keyboardEvents.colLetter = tableUI.alphabet.alphabet[tableUI.alphabet.alphabet.indexOf(this.className)];
+          if (tableUI.keyboardEvents.colLetter>'A'){
+            tableUI.keyboardEvents.colLetter = tableUI.alphabet.alphabet[tableUI.alphabet.alphabet.indexOf(tableUI.keyboardEvents.colLetter)-1];
+            tableUI.keyboardEvents.column = document.getElementsByClassName(tableUI.keyboardEvents.colLetter);
+            tableUI.keyboardEvents.rowNum = parseInt(this.parentElement.className);
+            tableUI.mouseEvents.cellClick ().bind(tableUI.keyboardEvents.column[tableUI.keyboardEvents.rowNum])();
+          }
+        } else if (event.keyCode === tableUI.keyCodes.RIGHT_ARROW) {
+          tableUI.mouseEvents.disableActive(this);
+          tableUI.keyboardEvents.colLetter = tableUI.alphabet.alphabet[tableUI.alphabet.alphabet.indexOf(this.className)+1];
+          tableUI.keyboardEvents.column = document.getElementsByClassName(tableUI.keyboardEvents.colLetter);
+          tableUI.keyboardEvents.rowNum = parseInt(this.parentElement.className);
+          tableUI.mouseEvents.cellClick ().bind(tableUI.keyboardEvents.column[tableUI.keyboardEvents.rowNum])();
+        }
+      };
+    }
+  },
+
+  scrollEvents: {
+    horisontalAdd: function () {
+      if (tableUI.table.node.clientWidth < document.body.scrollLeft + screen.width){
+        tableUI.addCol();
+      }
+    },
+    verticalAdd: function () {
+      if (tableUI.table.node.clientHeight < document.body.scrollTop + screen.height){
+        tableUI.addRow();
+      }
+    }
+  },
+
+  newTable: function (rows, cols){
+    this.create('table', 'tableDiv');
+    for (var i = 0; i <= rows; i += 1){
+      this.create('row', this.table.node);
+      this.row.node.className = i.toString();
+      for (var j=0; j <= cols; j += 1){
+        this.create('cell', this.row.node);
+        if (j >0) {
+          this.cell.node.className = this.alphabet.makeName(j-1, '');
+          if (i===0){
+            this.cell.node.innerHTML = this.cell.node.className;
+            this.cell.addEvent('click', this.mouseEvents.colClick());}
+        }
+        if (j > 0 && i > 0) {
+          this.cell.addEvent('click', this.mouseEvents.cellClick());
+          this.cell.addEvent('keydown', this.keyboardEvents.onCells());
+        } else {
+          if (i > 0) {this.cell.node.innerHTML = i;}
+          if (j === 0) {this.cell.addEvent('click', this.mouseEvents.rowClick());}
+        }
+      }
+    }
+    window.addEventListener("scroll", this.scrollEvents.horisontalAdd);
+    window.addEventListener("scroll", this.scrollEvents.verticalAdd);
+  },
+
+  create: function (element, parentElement) {
+      this[element].node = document.createElement(this[element].tag);
+      if (parentElement) {
+        if (typeof parentElement === 'string'){
+          document.getElementById(parentElement).appendChild(this[element].node);
+        } else if (parentElement.hasOwnProperty('node')){
+          parentElement.node.appendChild(this[element].node);
+        } else {
+          parentElement.appendChild(this[element].node);
+        }
+      }
+    },
+
+  addCol: function() {
+    var table = this.table.node;
+    var colLen = table.childNodes.length;
+    var rowLen = table.childNodes[0].childNodes.length;
+    var colName = this.alphabet.makeName(rowLen-1);
+    for (var i = 0; i < colLen; i += 1){
+      this.create('cell',this.table.node.childNodes[i]);
+      this.cell.node.className = colName;
+      if (i > 0){
+        this.cell.addEvent('click', this.mouseEvents.cellClick());
+        this.cell.addEvent('keydown', this.keyboardEvents.onCells());
+      }
+      if (i === 0){
+        this.cell.node.innerHTML = colName;
+        this.cell.addEvent('click', this.mouseEvents.colClick());
+      }
+    }
+  },
+
+  addRow: function() {
+    var table = this.table.node;
+    var colLen = table.childNodes.length;
+    var rowLen = table.childNodes[0].childNodes.length;
+    var colName = this.alphabet.makeName(rowLen-1);
+    this.create('row', table);
+    this.row.node.className = colLen.toString();
+    for (var i = 0; i < rowLen; i += 1){
+      this.create('cell', this.row.node);
+      if (i===0) {
+        this.cell.node.innerHTML = colLen.toString();
+        this.cell.addEvent('click', this.mouseEvents.rowClick());
+      } else {
+        this.cell.node.className = this.alphabet.makeName(i-1, '');
+        this.cell.addEvent('click', this.mouseEvents.cellClick());
+        this.cell.addEvent('keydown', this.keyboardEvents.onCells());
+      }
+
+    }
+
+  }
 };
 
-var table = new Element('table', 'table', null, 'table', 'tableDiv');
-var alphabet = 'abcdefghijklmnopqrstuvwx'.toUpperCase().split('');
+
+tableUI.newTable(40, 15);
 
 
-var rowName;
-var cellName;
-var cells = [];
-var lastCell;
-var lastCol;
-var lastRow;
 
-function cellClick (event){
-  return function (event) {
-      whiteAll (lastCol, lastRow, lastCell);
-      if (this.childNodes.length === 0){
-        var input = new Element('input', 'input', null, null, this);
-         input.node.focus();
-      } else {
-        console.log(this);
-        var text = this.childNodes[0].data;
-        this.removeChild(this.childNodes[0]);
-        var input = new Element('input', 'input', text, null, this);
-        input.node.focus();
-      }
-      this.style.backgroundColor='rgb(230,230,230)';
-      this.style.border='2px solid rgb(100,150,200)';
-      lastCell = this;
-    };
-}
-function buttonsPressCell (event){
-  var enter = 13;
-  var leftArrow = 37;
-  var upArrow = 38;
-  var rightArrow = 39;
-  var downArrow = 40;
-
-  return function (event) {
-    if (event.keyCode === enter) {
-      whiteCell(this);
-      console.log(this);
-    } else if (event.keyCode === downArrow) {
-      whiteCell(this);
-      var rowNum = parseInt(this.parentElement.className) + 1;
-      var column = document.getElementsByClassName(this.className);
-      cellClick ().bind(column[rowNum])();
-    } else if (event.keyCode === upArrow) {
-      whiteCell(this);
-      var rowNum = parseInt(this.parentElement.className);
-      if (rowNum > 1) {
-        rowNum -= 1;
-        var column = document.getElementsByClassName(this.className);
-        cellClick ().bind(column[rowNum])();
-      }
-    } else if (event.keyCode === leftArrow) {
-      whiteCell(this);
-      var colLetter = alphabet[alphabet.indexOf(this.className)];
-      console.log(colLetter>'A');
-      if (colLetter>'A'){
-        colLetter = alphabet[alphabet.indexOf(colLetter)-1];
-        var column = document.getElementsByClassName(colLetter);
-        var rowNum = parseInt(this.parentElement.className);
-        cellClick ().bind(column[rowNum])();
-      }
-    } else if (event.keyCode === rightArrow) {
-      whiteCell(this);
-      var colLetter = alphabet[alphabet.indexOf(this.className)+1];
-      var column = document.getElementsByClassName(colLetter);
-      var rowNum = parseInt(this.parentElement.className);
-      cellClick ().bind(column[rowNum])();
-    }
-  };
-}
-
-function whiteCol(lastNode){
-  var column = document.getElementsByClassName(lastNode.className);
-  for (var j=1;j <= column.length-1; j++){
-    column[j].style.backgroundColor='';
-  }
-}
-
-function whiteRow(lastNode){
-  lastNode.style.backgroundColor='';
-}
-
-function whiteCell(lastCell) {
-  lastCell.style.backgroundColor='';
-  lastCell.style.border='';
-  if (lastCell.childNodes[0]){
-    var text = lastCell.childNodes[0].value || lastCell.childNodes[0].data;
-    lastCell.removeChild(lastCell.childNodes[0]);
-    if (text) {
-      lastCell.appendChild(document.createTextNode(text));
-    }
-  }
-}
-
-function whiteAll (lastCol, lastRow, lastCell){
-  if (lastCell) {
-    whiteCell(lastCell);
-  }
-  if (lastCol) {
-    whiteCol(lastCol);
-  }
-  if (lastRow){
-    whiteRow(lastRow);
-  }
-}
-
-
-for (var i = 0; i<= 100; i++){
-  rowName = '' + i;
-  var tr = new Element(rowName, 'tr', null, rowName, table);
-  cells.push([]);
-
-  for (var j = 0; j<=23; j++){
-      cellName = alphabet[j-1];
-      var td = new Element(cellName, 'td', null, cellName, tr);
-      if (i>0 && j>0){
-        td.addEvent('click', cellClick(event));
-        td.addEvent('keydown', buttonsPressCell(event));
-      }
-      cells[i].push(td);
-      if (j===0){
-        if (i>=1)  {
-          td.node.innerHTML = i.toString();}}
-  }
-}
-
-
-cells[0].forEach(function(cell,i){
-  if (i>0){
-    cell.node.innerHTML=alphabet[i-1];
-    cell.addEvent('click', function(event){
-      whiteAll (lastCol, lastRow, lastCell);
-      for (var j=1;j <= cells.length-1; j++){
-        cells[j][i].node.style.backgroundColor='rgb(200,200,200)';
-      }
-      lastCol = this;
-    });
-    }
-  }
-  );
-
-cells.forEach(function(row, i){
-  if (i>0) {
-    row[0].addEvent('click', function(event){
-      whiteAll (lastCol, lastRow, lastCell);
-        this.parentElement.style.backgroundColor='rgb(200,200,200)';
-      lastRow = this.parentElement;
-    });
-  }
-});
-
-
-function getJSONData(path, callback) {
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function() {
-     if (httpRequest.readyState === 4) {
-          if (httpRequest.status === 200) {
-                console.log(httpRequest.responseText);
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-  httpRequest.open('GET', path);
-  httpRequest.send();
-}
-
-
-getJSONData('//localhost:3000/send', function(data){
-    console.log(data);
-console.log(data.a);
-});
